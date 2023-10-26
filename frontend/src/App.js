a86ee2d240ebea12e3bc3a65d2ac148c00b41673
cs-ion <asadrubina.ra@gmail.com>
2023-10-25 22:19:02 -0400

    testing the api key

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ function App() {
           {/* Add more navigation items here */}
         </ul>
       </nav>
+      {alert(API_KEY)}
       <Routes>
         <Route path="Property" element={<Property />} />
         <Route path="Broker" element={<Broker />} />
